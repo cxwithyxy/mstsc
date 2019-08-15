6924e2c5ad3581bb3b1c6a485c7ea5548b46d72d
chenxuan <523764720@qq.com>
2019-08-15 16:32:08 +0800

    现在能够通过命令行来打开mstsc了

--- index.py
+++ index.py
@@ -1,11 +1,20 @@
 import win32crypt
 import binascii
 import os
+import sys
 
-rdpUser = "cx"
-server_ip = "127.0.0.1"
+def get_argu(index: int, name: str =""):
+    try:
+        return sys.argv[index]
+    except:
+        print(f"ERROR: missing {name if len(name) > 1 else index}")
+        exit()
 
-pwdHash = win32crypt.CryptProtectData("1".encode("utf8"),'psw',None,None,None,0)
+server_ip = get_argu(1, "ip")
+rdpUser = get_argu(2, "username")
+password = get_argu(3, "password")
+
+pwdHash = win32crypt.CryptProtectData(password.encode("utf8"),'psw',None,None,None,0)
 rdpPwd = str(binascii.hexlify(pwdHash).decode("utf8")).upper()
 
 rdpTemp = open("rdp.txt").read()
