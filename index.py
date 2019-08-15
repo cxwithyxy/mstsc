bfc1a956051ff370d5a8cb40f1c8c6a81e4bddef
chenxuan <523764720@qq.com>
2019-08-15 16:50:05 +0800

    由于打包后路径变更, 因此需要通过 sys._MEIPASS 来获取实际路径

--- index.py
+++ index.py
@@ -2,6 +2,7 @@ import win32crypt
 import binascii
 import os
 import sys
+import time
 
 def get_argu(index: int, name: str =""):
     try:
@@ -14,16 +15,23 @@ server_ip = get_argu(1, "ip")
 rdpUser = get_argu(2, "username")
 password = get_argu(3, "password")
 
+base_path = ""
+try:
+    base_path = sys._MEIPASS
+except:
+    pass
+
 pwdHash = win32crypt.CryptProtectData(password.encode("utf8"),'psw',None,None,None,0)
 rdpPwd = str(binascii.hexlify(pwdHash).decode("utf8")).upper()
 
-rdpTemp = open("rdp.txt").read()
+rdpTemp = open(os.path.join(base_path, "rdp.txt")).read()
 
 rdpTemp += f"\nusername:s:{rdpUser}"
 rdpTemp += f"\npassword 51:b:{rdpPwd}"
 rdpTemp += f"\nfull address:s:{server_ip}"
 
-theRdpFile = open('theRdp.rdp', 'w', encoding="utf8")
+theRdp_path = os.path.join(base_path, 'theRdp.rdp')
+theRdpFile = open(theRdp_path, 'w', encoding="utf8")
 theRdpFile.write(rdpTemp)
 theRdpFile.close()
-os.system("mstsc theRdp.rdp /f")
+os.system(f"mstsc {theRdp_path} /f")
